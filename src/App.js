e9213ad7fb5570609cc4f5de2a303e075ee9b310
Jason <jason@Jasons-MacBook-Pro.local>
2019-11-18 15:50:02 -0800

    Add toggle between timers

--- src/App.js
+++ src/App.js
@@ -6,13 +6,52 @@ import LongBreak from './LongBreak.js';
 import tomato from './Tomato.gif'
 
 export default class App extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            showPomodoro: true,
+            showShortBreak: false,
+            showLongBreak: false
+        }
+    }
+
+    displayPomodoro = () => {
+        this.setState({
+            showPomodoro: true,
+            showShortBreak: false,
+            showLongBreak: false
+        })
+    }
+
+    displayShortBreak = () => {
+        this.setState({
+            showPomodoro: false,
+            showShortBreak: true,
+            showLongBreak: false
+        })
+    }
+
+    displayLongBreak = () => {
+        this.setState({
+            showPomodoro: false,
+            showShortBreak: false,
+            showLongBreak: true
+        })
+    }
+
     render(){
         return(
             <div className = 'backdrop'>
-                <Pomodoro/>
+                <h1>Tomodoro Timer</h1>
+                <button onClick = {this.displayPomodoro}>Pomodoro</button>
+                <button onClick = {this.displayShortBreak}>Short Break</button>
+                <button onClick = {this.displayLongBreak}>Long Break</button>
+                <br/>
+                <br/>
+                {this.state.showPomodoro && <Pomodoro/>}
+                {this.state.showShortBreak && <ShortBreak/>}
+                {this.state.showLongBreak && <LongBreak/>}
                 <img className = "tomato" src = {tomato}/>
-                <ShortBreak/>
-                <LongBreak/>
             </div>
         )
     }
